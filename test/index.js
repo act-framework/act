976226ee0ce963ac1307b1cdda3582f1f6e50e9e
Juan Lulkin <maiz@lulk.in>
2016-05-19 17:41:12 +0300

    Adds tests for main

--- test/index.js
+++ test/index.js
@@ -1,8 +1,53 @@
 import test from 'tape'
 import main from '../'
+import { spy } from 'sinon'
 
-test('main', (assert) => {
-  // TODO
+test('main: renders simple string', (assert) => {
+  const insertBefore = spy()
+  const mockNode = { insertBefore }
+
+  const { dom } = main('Hello world', {node: mockNode})
+  assert.equal(dom.tagName, 'SPAN')
+  assert.equal(dom.children[0].text, 'Hello world')
 
   assert.end()
 })
+
+test('main: renders simple tag', (assert) => {
+  const insertBefore = spy()
+  const mockNode = { insertBefore }
+
+  const { dom } = main(['h1', 'Hello world'], {node: mockNode})
+  assert.equal(dom.tagName, 'H1')
+  assert.equal(dom.children[0].text, 'Hello world')
+
+  assert.end()
+})
+
+test('main: renders simple function', (assert) => {
+  const insertBefore = spy()
+  const mockNode = { insertBefore }
+
+  const { dom } = main(() => ['h1', 'Hello world'], {node: mockNode})
+  assert.equal(dom.tagName, 'H1')
+  assert.equal(dom.children[0].text, 'Hello world')
+
+  assert.end()
+})
+
+test('main: renders updating function', (assert) => {
+  const insertBefore = spy()
+  const mockNode = { insertBefore }
+
+  const { dom, update } = main((name) => ['h1', `Hello ${name}`], {model: "Kripke", node: mockNode})
+  assert.equal(dom.tagName, 'H1')
+  assert.equal(dom.children[0].text, 'Hello Kripke')
+
+  update('name', 'Austin')
+  assert.equal(dom.tagName, 'H1')
+  assert.equal(dom.children[0].text, 'Hello Austin')
+
+  assert.end()
+})
+
+
