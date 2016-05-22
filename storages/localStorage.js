7a1fcb86527ad15fdc7e157181e9ce0b809730a9
Juan Lulkin <juan.lulkin@klarna.com>
2016-05-22 13:05:42 +0300

    Adds docs to localStorage

--- storages/localStorage.js
+++ storages/localStorage.js
@@ -1,7 +1,31 @@
-const get = () =>
-  JSON.parse(window.localStorage.getItem('act'))
+/**
+ * Stores and loads current state in applications.
+ *
+ * Only accepts JSON, so beware of simple strings & numbers.
+ * Used as an argument to `main`.
+ * @example
+ *   import localStorage from '@act/core/storages/localStorage'
+ *
+ *   main(view, { storage: localStorage }
+ */
 
-const set = (data) =>
-  window.localStorage.setItem('act', JSON.stringify(data))
+// TODO: check if localStorage is unique by tab/domain
+// else would have to improve storage key
+
+/**
+ * Gets the currently stored JSON
+ *
+ * @returns {json}
+ */
+const get = () =>
+  JSON.parse(window.localStorage.getItem('__act'))
+
+/**
+ * Sets the stored JSON
+ *
+ * @params {json} json
+ */
+const set = (json) =>
+  window.localStorage.setItem('__act', JSON.stringify(json))
 
 export default { get, set }
