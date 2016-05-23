b62e4204d926db1b8902b46d688fc2397286602d
Juan Lulkin <maiz@lulk.in>
2016-05-23 20:39:03 +0300

    Creates comprehensive list of events

---
+++ internals/events.js
@@ -0,0 +1,87 @@
+/**
+ * A list of DOM events, split by use-case.
+ */
+
+export const form = [
+  'blur',
+  'change',
+  // 'contextmenu', // not really supported
+  'focus',
+  'input',
+  'invalid',
+  'reset',
+  'search',
+  'select',
+  'submit'
+]
+
+export const keyboard = [
+  'keydown',
+  'keypress',
+  'keyup'
+]
+
+export const mouse = [
+  'click',
+  'dblclick',
+  'drag',
+  'dragend',
+  'dragenter',
+  'dragleave',
+  'dragover',
+  'dragstart',
+  'drop',
+  'mousedown',
+  'mousemove',
+  'mouseout',
+  'mouseover',
+  'mouseup',
+  'scroll',
+  'wheel'
+]
+
+// TODO: create some example to understand the use case
+export const clipboard = [
+  'copy',
+  'cut',
+  'paste'
+]
+
+export const media = [
+  'abort',
+  'canplay',
+  'canplaythrough',
+  'cuechange',
+  'durationchange',
+  'emptied',
+  'ended',
+  'error',
+  'loadeddata',
+  'loadedmetadata',
+  'loadstart',
+  'pause',
+  'play',
+  'progress',
+  'ratechange',
+  'seeked',
+  'seeking',
+  'stalled',
+  'suspend',
+  'timeupdate',
+  'volumechange',
+  'waiting'
+]
+
+export const misc = [
+  'show', // contextmenu
+  'toggle' // <details> tag
+]
+
+export default [
+  ...form,
+  ...keyboard,
+  ...mouse,
+  ...clipboard,
+  ...media,
+  ...misc
+]
