0f3fd5e535305a6efaf895fc7f8fbe51dbc052ca
Juan Lulkin <maiz@lulk.in>
2016-05-31 01:53:15 +0300

    Adds hmr module, overriding main

---
+++ hmr.js
@@ -0,0 +1,26 @@
+import _main from './'
+import TraversableHistory from './internals/TraversableHistory'
+
+const main = (view, opts) => {
+  const mod = opts.module
+  delete opts.module
+
+  const { node, history } = _main(view, { historyClass: TraversableHistory, ...opts })
+
+  if (mod.hot) {
+    mod.hot.accept()
+
+    if (mod.hot.data && mod.hot.data.history) {
+      history.reload(mod.hot.data.history)
+    }
+
+    mod.hot.dispose((data) => {
+      node.removeChild(node.firstChild)
+      data.history = history.timeline
+    })
+  }
+
+  return { node, history }
+}
+
+export default main
