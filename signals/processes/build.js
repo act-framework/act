9a6c752084c7bb70addf20a5817685b658a6dc45
Juan Lulkin <maiz@lulk.in>
2016-06-04 02:00:20 +0300

    Adds return to process build to be able to get a listener

--- signals/processes/build.js
+++ signals/processes/build.js
@@ -1,5 +1,5 @@
 import curry from 'ramda/src/curry'
 
 export default curry((builder, handler, eventSource, next) => {
-  eventSource((value) => builder(handler, next, value))
+  return eventSource((value) => builder(handler, next, value))
 })
