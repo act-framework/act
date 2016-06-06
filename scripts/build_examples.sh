82a11eb8664d541fd9951d9cd4027a47c560ac15
Juan Lulkin <maiz@lulk.in>
2016-06-07 00:41:48 +0300

    Remove unnecessary part of build_examples.sh

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -2,16 +2,6 @@
 
 ROOT=`pwd`
 
-EXAMPLES=animation/examples/
-
-for f in $EXAMPLES
-do
-  echo "Processing $ROOT/$f..."
-  cd "$ROOT/$f"
-  npm install
-  npm run build
-done
-
 EXAMPLES=examples/*/
 
 for f in $EXAMPLES
