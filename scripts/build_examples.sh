bddce449ade9a89a7c07b307a8f09d97807ca51a
Juan Lulkin <maiz@lulk.in>
2016-06-11 21:38:59 +0300

    Simplify build_examples script

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-ROOT=`pwd`
-
-EXAMPLES=examples/*/
-
-for f in $EXAMPLES
-do
-  echo "Processing $ROOT/$f..."
-  cd "$ROOT/$f"
-  npm install
-  npm run build
-done
-
+cd examples
+npm install
+npm run build
