abd5b5ecd1a27c2db3c4fc52fb5260be563c4743
Juan Lulkin <maiz@lulk.in>
2016-06-12 13:35:50 +0300

    Move general packages to root

--- scripts/publish_book.sh
+++ scripts/publish_book.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-mv .gitignore .master.gitignore
-mv .ghpages.gitignore .gitignore
-
 ./scripts/build_examples.sh
 npm run docs:clean
 npm run docs:build
 cd _book
 git init
-git commit --allow-empty -m 'update book'
 git checkout -b gh-pages
+git commit --allow-empty -m 'update book'
 git add .
 git commit -am 'update book'
 git push git@github.com:joaomilho/act gh-pages --force
 cd ..
-mv .gitignore .ghpages.gitignore
-mv .master.gitignore .gitignore
