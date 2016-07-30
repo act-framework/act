b1a86f6ba3203c0e15c1cd352baa53e21132e86c
Juan Lulkin <maiz@lulk.in>
2016-07-30 20:53:47 +0300

    Fix publish script
    Now examples work!

--- scripts/publish_book.sh
+++ scripts/publish_book.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+mv .gitignore .tmp-gitignore
+cp scripts/.gitignore .
 ./scripts/build_examples.sh
 npm run docs:clean
 npm run docs:build
+cp .gitignore _book
 cd _book
 git init
 git checkout -b gh-pages
@@ -11,3 +14,4 @@ git add .
 git commit -am 'update book'
 git push git@github.com:joaomilho/act gh-pages --force
 cd ..
+mv .tmp-gitignore .gitignore
